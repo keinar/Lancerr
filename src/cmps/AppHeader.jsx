3899e6b6833ad42692de3050bb9dcd3cae72aebc
keinar <kelkayam@seatgeek.com>
2024-01-22 00:59:31 +0200

    Adding categories menu

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,7 +1,41 @@
-import { Menu, Search } from "lucide-react";
-import React from "react";
+import {
+  ArrowLeft,
+  ArrowRight,
+  ChevronLeft,
+  ChevronRight,
+  Menu,
+  Search,
+} from "lucide-react";
+import React, { useEffect, useState } from "react";
 
 export function AppHeader() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isOnTop, setisOnTop] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleIsOnTop);
+
+    return () => {
+      window.removeEventListener("scroll", handleIsOnTop);
+    };
+  }, []);
+
+  function handleIsOnTop() {
+    window.scrollY > 100 ? setisOnTop(true) : setisOnTop(false);
+  }
+
+  const handleRightClick = () => {
+    const categoriesMenu = document.getElementById("categories-menu-package");
+    categoriesMenu.scrollBy({ left: 200, behavior: "smooth" });
+    setIsScrolled(true);
+  };
+
+  const handleLeftClick = () => {
+    const categoriesMenu = document.getElementById("categories-menu-package");
+    categoriesMenu.scrollBy({ left: -200, behavior: "smooth" });
+    setIsScrolled(false);
+  };
+
   return (
     <div className="app-header">
       <div className="navbar">
@@ -13,7 +47,7 @@ export function AppHeader() {
           <div className="logo-dot">.</div>
         </div>
 
-        <div className="search-form-wrapper">
+        <div className={`search-form-wrapper ${isOnTop ? " display" : ""} `}>
           <form>
             <input
               type="search"
@@ -26,7 +60,7 @@ export function AppHeader() {
               placeholder="What service are you looking for today?"
             />
             <button className="submit-button">
-              <Search size={15} color="white" />
+              <Search size={18} color="white" />
             </button>
           </form>
         </div>
@@ -39,6 +73,41 @@ export function AppHeader() {
           </ul>
         </nav>
       </div>
+      {isOnTop && (
+        <div
+          className={`categories-menu-wrapper ${isScrolled ? "scrolled" : ""}`}
+        >
+          <button
+            className="scroll-button left-scroll"
+            onClick={handleLeftClick}
+          >
+            <ChevronLeft color="#62646a" />
+          </button>
+          <nav
+            id="categories-menu-package"
+            className="categories-menu-package default has-overflow"
+          >
+            <ul className="categories">
+              <li>Graphics & Design</li>
+              <li>Programming & Tech</li>
+              <li>Digital Marketing</li>
+              <li>Video & Animation</li>
+              <li>Writing & Translation</li>
+              <li>Music & Audio</li>
+              <li>Business</li>
+              <li>Data</li>
+              <li>Photography</li>
+              <li>AI Services</li>
+            </ul>
+          </nav>
+          <button
+            className="scroll-button right-scroll"
+            onClick={handleRightClick}
+          >
+            <ChevronRight color="#62646a" />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
