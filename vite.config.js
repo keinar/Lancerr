edcd9376c3274de3bbf2f02b89d4e2db5a8285ea
keinar <kelkayam@seatgeek.com>
2024-01-22 18:12:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/react-vite-gh-pages/";
-  }
-
-  return config;
+export default defineConfig({
+  base: "/Lancerr/",
+  plugins: [react()],
 });
