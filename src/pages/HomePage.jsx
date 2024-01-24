0fea4e0f409524a412c17085efc5f8025d9dc6c7
keinar <kelkayam@seatgeek.com>
2024-01-24 14:24:56 +0200

    Home page hero design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react";
+import { Search, SearchIcon } from "lucide-react";
 import React from "react";
 import googleSvg from "../assets/imgs/google.129f8ec.svg";
 import metaSvg from "../assets/imgs/meta.398bc1c.svg";
@@ -19,123 +19,22 @@ export function HomePage() {
 
   return (
     <div className="home-page">
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      <p>home page</p>
-      {/* <div className="hero">
-        <div className="hero-content">
+      <div className="hp-hero">
+        <div className="hp-hero-content">
           <h1>
-            Find the right <span>freelance</span> service, right away
+            Find the right <i>freelance</i> service, right away
           </h1>
-
-          <form className="search-form-home">
-            <input
-              type="search"
-              className="long-placeholder"
-              placeholder="Search for any service..."
-            />
-            <button>
-              <div className="submit-button-icon">
-                <Search size={20} color="white" />
-              </div>
-            </button>
-          </form>
-
-          <div className="trusted-by">
-            <span>Trusted by:</span>
-            <ul>
-              <li>
-                <img src={metaSvg} alt="meta logo" />
-              </li>
-              <li>
-                <img src={googleSvg} alt="google logo" />
-              </li>
-              <li>
-                <img src={netflixSvg} alt="netflix logo" />
-              </li>
-              <li>
-                <img src={pgSvg} alt="pg logo" />
-              </li>
-              <li>
-                <img src={paypalSvg} alt="paypal logo" />
-              </li>
-            </ul>
+          <div className="hp-hero-search-bar">
+            <form className="hp-search-form">
+              <input type="search" placeholder="Search for any service..." />
+              <button className="inside-button">
+                <SearchIcon size={16} color="white" />
+              </button>
+            </form>
           </div>
         </div>
+        <div className="hp-hero-trusted"></div>
       </div>
-
-      <div className="categories-blocks">
-        <div className="category-block">
-          <p>Graphics & Design</p>
-        </div>
-        <div className="category-block">
-          <p>AI Services</p>
-        </div>
-        <div className="category-block">
-          <p>Digital Marketing</p>
-        </div>
-        <div className="category-block">
-          <p>Writing & Translation</p>
-        </div>
-        <div className="category-block">
-          <p>Video & Animation</p>
-        </div>
-        <div className="category-block">
-          <p>Music & Audio</p>
-        </div>
-        <div className="category-block">
-          <p>Programming & Tech</p>
-        </div>
-        <div className="category-block">
-          <p>Business</p>
-        </div>
-        <div className="category-block">
-          <p>Data</p>
-        </div>
-      </div>
-
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-      <h2>Popular services</h2> */}
     </div>
   );
 }
