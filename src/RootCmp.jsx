d0ea43f740444e32b3c2f6f9b99e64e352889fb7
keinar <kelkayam@seatgeek.com>
2024-01-24 14:24:56 +0200

    Home page hero design

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router";
 
 import routes from "./routes";
@@ -6,9 +6,8 @@ import routes from "./routes";
 import { AppHeader } from "./cmps/AppHeader/AppHeader";
 import { UserDetails } from "./pages/UserDetails";
 import { AppFooter } from "./cmps/AppFooter/AppFooter";
-import {loadGigs} from "./store/actions/gig.actions"
+import { loadGigs } from "./store/actions/gig.actions";
 export function RootCmp() {
-
   useEffect(() => {
     // Sanitize filterBy
     loadGigs();
@@ -17,19 +16,17 @@ export function RootCmp() {
   return (
     <div className="root">
       <AppHeader />
-      <main className="main-container">
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              exact={true}
-              element={route.component}
-              path={route.path}
-            />
-          ))}
-          <Route path="user/:id" element={<UserDetails />} />
-        </Routes>
-      </main>
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            exact={true}
+            element={route.component}
+            path={route.path}
+          />
+        ))}
+        <Route path="user/:id" element={<UserDetails />} />
+      </Routes>
       <AppFooter />
     </div>
   );
