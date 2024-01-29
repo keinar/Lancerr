f038fc4b8e4b7212661b9842979ed6765a946c0f
keinar <kelkayam@seatgeek.com>
2024-01-29 20:32:38 +0200

    HOME PAGE INIT

---
+++ src/pages/HomePage/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-backgrounds">
+        <div className="hero-background hero-jenny"></div>
+        <div className="hero-background hero-colin"></div>
+        <div className="hero-background hero-scarlett"></div>
+        <div className="hero-background hero-jordan"></div>
+      </div>
+    </section>
+  );
+}
