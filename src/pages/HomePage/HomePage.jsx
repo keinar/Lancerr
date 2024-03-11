6b4f516e6e39d1beb2f4232b14d1818c7d98caaa
keinar <kelkayam@seatgeek.com>
2024-03-11 14:18:09 +0200

    Continue homePage design and implementation

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,8 @@
-// import { Search, SearchIcon } from "lucide-react";
-// import React from "react";
-// import googleSvg from "../assets/imgs/google.129f8ec.svg";
-// import metaSvg from "../assets/imgs/meta.398bc1c.svg";
-// import netflixSvg from "../assets/imgs/netflix.e1b0070.svg";
-// import paypalSvg from "../assets/imgs/paypal.0520267.svg";
-// import pgSvg from "../assets/imgs/pg.a47f1ab.svg";
+import { Search, SearchIcon } from "lucide-react"
+import React from "react"
 
-import { SearchIcon } from "lucide-react"
 import Hero from "./Hero"
+import TrustedBy from "./TrustedBy"
 
 export function HomePage() {
   // Mock data for tags
@@ -16,6 +11,10 @@ export function HomePage() {
   return (
     <main className="home-page">
       <Hero allTags={allTags} />
+      <TrustedBy />
+      <div className="popular-services">
+        <h2>Popular Services</h2>
+      </div>
     </main>
   )
 }
