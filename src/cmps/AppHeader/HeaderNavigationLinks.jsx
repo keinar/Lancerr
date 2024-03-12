06829959e2a87bd6eb6b725183862e3c2d5f9f8d
keinar <kelkayam@seatgeek.com>
2024-03-13 00:34:43 +0200

    Dialog design & implement

--- src/cmps/AppHeader/HeaderNavigationLinks.jsx
+++ src/cmps/AppHeader/HeaderNavigationLinks.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import Login from "./Login"
 import { loadUsers, logout, setLoggedinUser } from "../../store/actions/user.actions"
+import { Check } from "lucide-react"
 
 export default function HeaderNavigationLinks() {
   const [isAuthDialogOpen, setIsAuthDialogOpen] = useState(false)
@@ -39,10 +40,37 @@ export default function HeaderNavigationLinks() {
           Join
         </li>
         {isAuthDialogOpen && !loggedinUser && (
-          <dialog open={isAuthDialogOpen} className="auth-dialog">
-            <Login loggedinUser={loggedinUser} setLoggedinUser={setLoggedinUser} />
-            <button onClick={closeAuthDialog}>Close</button>
-          </dialog>
+          <div className="auth-dialog-container">
+            <dialog open={isAuthDialogOpen} className="auth-dialog">
+              <section className="dialog-left">
+                <h2>Success starts here</h2>
+                <ul>
+                  <li>
+                    <span>
+                      <Check size={15} />
+                    </span>
+                    Over 600 categories
+                  </li>
+                  <li>
+                    <span>
+                      <Check size={15} />
+                    </span>
+                    Pay per project, not per hour
+                  </li>
+                  <li>
+                    <span>
+                      <Check size={15} />
+                    </span>
+                    Access to talent and businesses across the globe
+                  </li>
+                </ul>
+              </section>
+              <section className="dialog-right">
+                <Login loggedinUser={loggedinUser} setLoggedinUser={setLoggedinUser} />
+                <button onClick={closeAuthDialog}>Close</button>
+              </section>
+            </dialog>
+          </div>
         )}
       </ul>
     </nav>
