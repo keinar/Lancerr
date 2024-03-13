985d535c99917aeaa4edd486e85b43e6fcecca98
Yuval Neumann <yuvalno@gmail.com>
2024-03-13 07:49:29 +0200

    Moved the filter from buttons to the navigation bar, and replaced gigFilter with HeaderCategoriesMenu in the AppHeader component

--- src/cmps/GigFilter.jsx
+++ src/cmps/GigFilter.jsx
@@ -1,29 +1,46 @@
 import { useEffect, useState } from "react"
+import { setFilterBy } from "../store/actions/gig.actions.js"
+import {store} from "../store/store.js"
+import { useNavigate } from "react-router"
 
-export function GigFilter({  onSetFilter }) {
+export function GigFilter() {
 
-    const [filterByToEdit, setFilterByToEdit] = useState()
+    // const [filterByToEdit, setFilterByToEdit] = useState()
+    
+    const filterBy = store.getState().gigModule.filterBy  
+    const navigate = useNavigate()
+    
 
-
-    useEffect(() => {
-        onSetFilter(filterByToEdit)
-    }, [filterByToEdit])
+    // useEffect(() => {
+    //     onSetFilter(filterByToEdit)
+    // }, [filterByToEdit])
+     useEffect(() => {
+        
+        
+     },[])
 
     function handleTagClick(tag) {
+        
+        
         // Create a new filter with the clicked tag
         const updatedFilter = {
-            ...filterByToEdit,
+            // ...filterByToEdit,
             tags: [tag], // Replace the existing tags with the clicked tag
         }
-        setFilterByToEdit(updatedFilter)
+        const fieldsToUpdate = { ...filterBy, ...updatedFilter }
+        setFilterBy(fieldsToUpdate)
+        navigate('/gig')
+        // setFilterByToEdit(updatedFilter)
     }
 
-
     return (
-        <section className="gig-filter">
-            <div className="static">
-                <ul className="categories-container">
-                    <button
+        // <section className="gig-filter">
+        <nav
+        id="categories-menu-package"
+        className="categories-menu-package default has-overflow"
+      >
+                <ul className="categories">
+                    {/* <button
                         className="category-button"
                         onClick={() => handleTagClick("logo-design")}
                     >
@@ -40,9 +57,22 @@ export function GigFilter({  onSetFilter }) {
                         onClick={() => handleTagClick("Digital Marketing")}
                     >
                         Digital Marketing
-                    </button>
+                    </button> */}
+                    <li onClick={() => handleTagClick("logo-design")}>Graphics & Design
+
+                    </li>
+                    <li onClick={() => handleTagClick("Programming & Tech")}>Programming & Tech</li>
+                    <li onClick={() => handleTagClick("Digital Marketing")}>Digital Marketing</li>
+                    <li onClick={() => handleTagClick("Video & Animation")}>Video & Animation</li>
+                    <li onClick={() => handleTagClick("Writing & Translation")}>Writing & Translation</li>
+                    <li onClick={() => handleTagClick("Music & Audio")}>Music & Audio</li>
+                    <li onClick={() => handleTagClick("Business")}>Business</li>
+                    <li onClick={() => handleTagClick("Data")}>Data</li>
+                    <li onClick={() => handleTagClick("Photography")}>Photography</li>
+                    <li onClick={() => handleTagClick("AI Services")}>AI Services</li>
                 </ul>
-            </div>
-        </section>
+                </nav>
+            
+        // </section>
     )
 }
