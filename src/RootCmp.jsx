fcabeea9a4717bc4a7ad462a026e3b8035d952bd
keinar <kelkayam@seatgeek.com>
2024-03-15 00:14:34 +0200

    Update gig path to explore and design the header on login

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,33 +1,28 @@
-import React, { useEffect } from "react";
-import { Routes, Route } from "react-router";
+import React, { useEffect } from "react"
+import { Routes, Route } from "react-router"
 
-import routes from "./routes";
+import routes from "./routes"
 
-import { AppHeader } from "./cmps/AppHeader/AppHeader";
-import { UserDetails } from "./pages/UserDetails";
-import { AppFooter } from "./cmps/AppFooter/AppFooter";
-import { loadGigs } from "./store/actions/gig.actions";
+import { AppHeader } from "./cmps/AppHeader/AppHeader"
+import { UserDetails } from "./pages/UserDetails"
+import { AppFooter } from "./cmps/AppFooter/AppFooter"
+import { loadGigs } from "./store/actions/gig.actions"
 export function RootCmp() {
   useEffect(() => {
     // Sanitize filterBy
-    loadGigs();
-  }, []);
+    loadGigs()
+  }, [])
 
   return (
     <div>
       <AppHeader />
       <Routes>
-        {routes.map((route) => (
-          <Route
-            key={route.path}
-            exact={true}
-            element={route.component}
-            path={route.path}
-          />
+        {routes.map(route => (
+          <Route key={route.path} exact={true} element={route.component} path={route.path} />
         ))}
         <Route path="user/:id" element={<UserDetails />} />
       </Routes>
       <AppFooter />
     </div>
-  );
+  )
 }
--- src/cmps/GigFilter.jsx
+++ src/cmps/GigFilter.jsx
@@ -1,57 +1,45 @@
 import { useEffect, useState } from "react"
 import { setFilterBy } from "../store/actions/gig.actions.js"
-import {store} from "../store/store.js"
+import { store } from "../store/store.js"
 import { useNavigate } from "react-router"
 
 export function GigFilter() {
+  // const [filterByToEdit, setFilterByToEdit] = useState()
 
-    // const [filterByToEdit, setFilterByToEdit] = useState()
-    
-    const filterBy = store.getState().gigModule.filterBy  
-    const navigate = useNavigate()
-    
+  const filterBy = store.getState().gigModule.filterBy
+  const navigate = useNavigate()
 
-    // useEffect(() => {
-    //     onSetFilter(filterByToEdit)
-    // }, [filterByToEdit])
-     useEffect(() => {
-        
-        
-     },[])
+  // useEffect(() => {
+  //     onSetFilter(filterByToEdit)
+  // }, [filterByToEdit])
+  useEffect(() => {}, [])
 
-    function handleTagClick(tag) {                
-        // Create a new filter with the clicked tag
-        const updatedFilter = {
-            // ...filterByToEdit,
-            tags: [tag], // Replace the existing tags with the clicked tag
-        }        
-        const fieldsToUpdate = { ...filterBy, ...updatedFilter }
-        setFilterBy(fieldsToUpdate)
-        navigate('/gig')
-        // setFilterByToEdit(updatedFilter)
+  function handleTagClick(tag) {
+    // Create a new filter with the clicked tag
+    const updatedFilter = {
+      // ...filterByToEdit,
+      tags: [tag], // Replace the existing tags with the clicked tag
     }
+    const fieldsToUpdate = { ...filterBy, ...updatedFilter }
+    setFilterBy(fieldsToUpdate)
+    navigate("/explore")
+    // setFilterByToEdit(updatedFilter)
+  }
 
-    return (        
-        <nav
-        id="categories-menu-package"
-        className="categories-menu-package default has-overflow"
-      >
-                <ul className="categories">
-                    <li onClick={() => handleTagClick("logo-design")}>Graphics & Design
-
-                    </li>
-                    <li onClick={() => handleTagClick("Programming & Tech")}>Programming & Tech</li>
-                    <li onClick={() => handleTagClick("Digital Marketing")}>Digital Marketing</li>
-                    <li onClick={() => handleTagClick("Video & Animation")}>Video & Animation</li>
-                    <li onClick={() => handleTagClick("Writing & Translation")}>Writing & Translation</li>
-                    <li onClick={() => handleTagClick("Music & Audio")}>Music & Audio</li>
-                    <li onClick={() => handleTagClick("Business")}>Business</li>
-                    <li onClick={() => handleTagClick("Data")}>Data</li>
-                    <li onClick={() => handleTagClick("Photography")}>Photography</li>
-                    <li onClick={() => handleTagClick("AI Services")}>AI Services</li>
-                </ul>
-                </nav>
-            
-        
-    )
+  return (
+    <nav id="categories-menu-package" className="categories-menu-package default has-overflow">
+      <ul className="categories">
+        <li onClick={() => handleTagClick("logo-design")}>Graphics & Design</li>
+        <li onClick={() => handleTagClick("Programming & Tech")}>Programming & Tech</li>
+        <li onClick={() => handleTagClick("Digital Marketing")}>Digital Marketing</li>
+        <li onClick={() => handleTagClick("Video & Animation")}>Video & Animation</li>
+        <li onClick={() => handleTagClick("Writing & Translation")}>Writing & Translation</li>
+        <li onClick={() => handleTagClick("Music & Audio")}>Music & Audio</li>
+        <li onClick={() => handleTagClick("Business")}>Business</li>
+        <li onClick={() => handleTagClick("Data")}>Data</li>
+        <li onClick={() => handleTagClick("Photography")}>Photography</li>
+        <li onClick={() => handleTagClick("AI Services")}>AI Services</li>
+      </ul>
+    </nav>
+  )
 }
