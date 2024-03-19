b296a8d4f60f8b7175fec45e687dbd989ca75024
gal1606 <galpersonalcloud@gmail.com>
2024-03-19 20:58:00 +0200

    PackTabs fix - switching tabs works. flags component to generate reviews flags dynamically

---
+++ src/cmps/flags.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import china from "../assets/imgs/china.png";
+import thailand from "../assets/imgs/thailand.png";
+import germany from "../assets/imgs/germany.png";
+import uk from "../assets/imgs/uk.png";
+import usa from "../assets/imgs/usa.png";
+
+export function getFlagImage(country) {
+  switch(country) {
+    case "China":
+      return china;
+    case "Thailand":
+      return thailand;
+    case "Germany":
+      return germany;
+    case "United Kingdom":
+      return uk;
+    case "United States":
+      return usa;
+    default:
+      // Return a default image or handle other cases as needed
+      return null;
+  }
+}
