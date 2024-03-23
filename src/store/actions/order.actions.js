00320091a4945a51d3dbf9d84336f43c339edb28
Yuval Neumann <yuvalno@gmail.com>
2024-03-23 19:17:11 +0200

    Implemented user profile page with seller orders and functionality for approving orders and updating order status

--- src/store/actions/order.actions.js
+++ src/store/actions/order.actions.js
@@ -2,12 +2,12 @@ import { store } from '../store.js'
 import * as actions from '../reducers/order.reducer.js'
 import {ADD_ORDER, SET_ORDERS, UPDATE_ORDER} from "../reducers/order.reducer.js";
 import { orderService } from '../../services/order.service.local.js'
-
+import {sellerOrderService} from '../../services/sellerOrder.service.local.js'
 
 export async function saveOrder(orderToSave) {
     // store.dispatch({ type: SET_IS_LOADING, isLoading: true })
-    const type = orderToSave.id ? UPDATE_ORDER : ADD_ORDER
-    try {
+    const type = orderToSave._id ? UPDATE_ORDER : ADD_ORDER
+    try { 
         const savedOrder = await orderService.save(orderToSave)
         store.dispatch({ type, order: savedOrder })
     } catch (err) {
@@ -19,19 +19,26 @@ export async function saveOrder(orderToSave) {
 }
 
 
-export async function loadOrders() {
-    const loggedInUser = JSON.parse(sessionStorage.getItem('loggedinUser'))
-    const filterBy = { loggedinUser: loggedInUser._id } // Initialize filterBy with the logged-in user's ID
-    try {
-      const orders = await orderService.query(filterBy)
-      store.dispatch({ type: SET_ORDERS, orders })
-    } catch (err) {
-      console.log('Had issues loading orders', err)
-      throw err;
-    } finally {
-      // store.dispatch({ type: 'SET_IS_LOADING', isLoading: false })
+export async function loadOrders(isSeller = false) {
+  const loggedInUser = JSON.parse(sessionStorage.getItem('loggedinUser'));
+  const filterBy = { loggedinUser: loggedInUser._id };
+
+  try {
+    let orders;
+    if (isSeller) {
+      orders = await sellerOrderService.query(filterBy);
+    } else {
+      orders = await orderService.query(filterBy);
     }
+
+    store.dispatch({ type: SET_ORDERS, orders });
+  } catch (err) {
+    console.log('Had issues loading orders', err);
+    throw err;
+  } finally {
+    // store.dispatch({ type: 'SET_IS_LOADING', isLoading: false })
   }
+}
 
 export function onToggleModal(modalData = null) {
     store.dispatch({
