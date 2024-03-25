01f05660ef6d4d038c8a12bde4488b0b2fd0485d
keinar <kelkayam@seatgeek.com>
2024-03-25 22:55:22 +0200

    Adding selling proposition section to the home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,27 @@
-import { Search, SearchIcon } from "lucide-react"
-import React, { useEffect } from "react"
 
+import React, { useEffect } from "react"
 import Hero from "./Hero"
 import TrustedBy from "./TrustedBy"
 import PopularServicesCarousel from "./PopularServicesCarousel"
 import {  setFilterBy } from "../../store/actions/gig.actions.js"
+import SellingProposition from "./SellingProposition.jsx"
 
 export function HomePage() {
 
   useEffect(()=>{
         setFilterBy([])
   }, [])
+  
   return (
     <main className="home-page">
       <Hero />
       <TrustedBy />
-      <div className="popular-services main-container">
+      <div className="popular-services">
+        <div className="popular-services-container main-container">
         <h2>Popular Services</h2>
         <PopularServicesCarousel />
+        </div>
+       <SellingProposition/>
       </div>
     </main>
   )
