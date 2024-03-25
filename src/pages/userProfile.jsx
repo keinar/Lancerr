697896093eefa129d806e0cd4f7a3441a62855e7
keinar <kelkayam@seatgeek.com>
2024-03-25 12:21:04 +0200

    Update layout

--- src/pages/userProfile.jsx
+++ src/pages/userProfile.jsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
 import { loadOrders } from "../store/actions/order.actions.js"
-import { store } from "../store/store.js"
-import { Link } from "react-router-dom"
 import { SellerOrdersList } from "../cmps/SellerOrderList.jsx"
 
 export function UserProfile() {
 
   const orders = useSelector(storeState => storeState.orderModule.orders)
-
+ const owner = useSelector(storeState => storeState.userModule.user)
   useEffect(() => {
     loadOrders(true)
   }, [])
@@ -21,7 +19,15 @@ export function UserProfile() {
         <section className="details-container">
           <section className="user-details">
             <div className="user-card">
-              <div className="user-profile-info"></div>
+              <div className="user-profile-info">
+                <div className="user-img">
+                  <img src={owner.imgUrl} />
+                  </div>
+                < div className="user-profile-label">
+                  <h2>{owner.fullname}</h2>
+                  {/* <h3>@{owner.username}</h3> */}
+                </div>
+              </div>
             </div>
             <div className="desc-card">
 
@@ -48,69 +54,4 @@ export function UserProfile() {
       </main>
     </main>
   )
-  // return (
-  //   <main className="main-container">
-  //     <section className="buyer-orders-container full">
-  //       <div className="buyer-orders main-container">
-  //         <div className="user-orders">
-  //           <h1 className="header">Manage Orders</h1>
-  //         </div>
-  //         {orders.length !== 0 ? (
-  //           <section className="orders-layout">
-  //             {orders.map(order => {
-  //               // Calculate due date dynamically
-  //               const orderDate = new Date(order.orderDate)
-  //               const dueDate = new Date(orderDate)
-  //               dueDate.setDate(dueDate.getDate() + order.gig.daysToMake)
-  //               const formattedDueDate = dueDate.toLocaleString("default", {
-  //                 month: "short",
-  //                 day: "numeric",
-  //                 year: "numeric",
-  //               })
-
-  //               return (
-  //                 <section className="order-card-container flex" key={order._id}>
-  //                   <div className="order-info">
-  //                     <h5 className="card-header">Order status</h5>
-  //                     <h4 className="status flex pending">{order.status}</h4>
-  //                     <p className="date">Due date on {formattedDueDate}</p>
-  //                     <div className="progress-bar">
-  //                       <div className="progress progress-25" style={{ width: "25%" }}></div>
-  //                     </div>
-  //                   </div>
-  //                   <div className="gig flex">
-  //                     <div className="gig-img">
-  //                       <img src={order.gig.imgUrl} alt="Gig Picture" />
-  //                     </div>
-  //                     <div className="gig-info">
-  //                       <h4 className="gig-title">{order.gig.title}</h4>
-  //                       <h5>{order.gig.tags[0]}</h5>
-  //                       <h5 className="seller">
-  //                         From <span className="click">{order.seller.fullname}</span>
-  //                       </h5>
-  //                     </div>
-  //                   </div>
-  //                   <div className="order-data">
-  //                     <div className="order">
-  //                       <h3>Order no.</h3>
-  //                       <h3># {order.gig._id} </h3>
-  //                     </div>
-  //                     <div className="order">
-  //                       <h3>Delivery time</h3>
-  //                       <h3>{order.gig.daysToMake} DAYS</h3>
-  //                     </div>
-  //                   </div>
-  //                 </section>
-  //               )
-  //             })}
-  //           </section>
-  //         ) : (
-  //           <div>
-  //             <p>No orders  To Manage yet</p>                              
-  //           </div>
-  //         )}
-  //       </div>
-  //     </section>
-  //   </main>
-  // )
 }
